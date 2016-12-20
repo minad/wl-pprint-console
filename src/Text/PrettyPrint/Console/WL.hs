f0626e7407ff80e6949d4eb6d0e5a64d0837dca4
Daniel Mendler <mail@daniel-mendler.de>
2016-12-20 08:51:53 +0100

    use Text instead of String

--- src/Text/PrettyPrint/Console/WL.hs
+++ src/Text/PrettyPrint/Console/WL.hs
@@ -15,7 +15,7 @@
 -----------------------------------------------------------
 
 module Text.PrettyPrint.Console.WL (
-  module Text.PrettyPrint.Annotated.WL
+  module Text.PrettyPrint.Annotated.WL.Text
 
   -- * Display documents annotated with pairs of strings
   , displayWrapped, displayWrappedT, displayWrappedB, displayWrappedS
@@ -33,10 +33,14 @@ module Text.PrettyPrint.Console.WL (
   , displayB
 ) where
 
-import Text.PrettyPrint.Annotated.WL
+import Text.PrettyPrint.Annotated.WL.Text
 import System.Console.Style
 import Control.Monad.Trans
-import System.IO (Handle, hPutStr, stdout)
+import System.IO (Handle, stdout)
+import Data.Text (Text)
+import qualified Data.Text as T
+import qualified Data.Text.Encoding as T
+import qualified Data.Text.IO as T
 import qualified Data.Text.Lazy as TL
 import qualified Data.Text.Lazy.Builder as TL
 import qualified Data.ByteString.Lazy as BL
@@ -46,56 +50,57 @@ import qualified Data.ByteString.Builder as BL
 -- rendering function and outputs a 'ByteString'. Along the way, all annotations are
 -- discarded.
 displayB :: SimpleDoc a -> BL.ByteString
-displayB = BL.toLazyByteString . displayDecorated cm cm BL.stringUtf8
+displayB = BL.toLazyByteString . displayDecorated cm cm T.encodeUtf8Builder
  where cm = const mempty
 
 -- | Escape a HTML string by replacing special characters with HTML entities.
-escapeHTML :: String -> String
-escapeHTML = concatMap $ \c ->
-  case c of
-    '"' -> "&quot;"
-    '&' -> "&amp;"
-    '<' -> "&lt;"
-    '>' -> "&gt;"
-    _   -> [c]
+escapeHTML :: Text -> Text
+escapeHTML = id
+-- concatMap $ \c ->
+--   case c of
+--     '"' -> "&quot;"
+--     '&' -> "&amp;"
+--     '<' -> "&lt;"
+--     '>' -> "&gt;"
+--     _   -> [c]
 
--- | Display a rendered document which is annotated with pairs of strings @(String,String)@ and
+-- | Display a rendered document which is annotated with pairs of strings @(Text,Text)@ and
 -- output a 'Monoid'.
 --
 -- The first element of the pair is prepended to the annotated region,
 -- the second after the annotated region.
-displayWrapped :: Monoid o => (String -> o) -> SimpleDoc (String, String) -> o
+displayWrapped :: Monoid o => (Text -> o) -> SimpleDoc (Text, Text) -> o
 displayWrapped f = displayDecorated (f . fst) (f . snd) f
 
--- | Display a rendered document which is annotated with pairs of strings @(String,String)@ and
+-- | Display a rendered document which is annotated with pairs of strings @(Text,Text)@ and
 -- output 'Text'.
 --
 -- The first element of the pair is prepended to the annotated region,
 -- the second after the annotated region.
-displayWrappedT :: SimpleDoc (String, String) -> TL.Text
-displayWrappedT = TL.toLazyText . displayWrapped TL.fromString
+displayWrappedT :: SimpleDoc (Text, Text) -> TL.Text
+displayWrappedT = TL.toLazyText . displayWrapped TL.fromText
 
--- | Display a rendered document which is annotated with pairs of strings @(String,String)@ and
+-- | Display a rendered document which is annotated with pairs of strings @(Text,Text)@ and
 -- output 'ByteString'.
 --
 -- The first element of the pair is prepended to the annotated region,
 -- the second after the annotated region.
-displayWrappedB :: SimpleDoc (String, String) -> BL.ByteString
-displayWrappedB = BL.toLazyByteString . displayWrapped BL.stringUtf8
+displayWrappedB :: SimpleDoc (Text, Text) -> BL.ByteString
+displayWrappedB = BL.toLazyByteString . displayWrapped T.encodeUtf8Builder
 
--- | Display a rendered document which is annotated with pairs of strings @(String,String)@ and
+-- | Display a rendered document which is annotated with pairs of strings @(Text,Text)@ and
 -- output a 'ShowS' function.
 --
 -- The first element of the pair is prepended to the annotated region,
 -- the second after the annotated region.
-displayWrappedS :: SimpleDoc (String, String) -> ShowS
-displayWrappedS = displayDecoratedA ((++) . fst) ((++) . snd) (++)
+displayWrappedS :: SimpleDoc (Text, Text) -> ShowS
+displayWrappedS = displayDecoratedA ((++) . T.unpack . fst) ((++) . T.unpack . snd) ((++) . T.unpack)
 
 -- | Display a rendered document as HTML and output a 'Monoid'.
 --
 -- The annotated region is wrapped by @<span class="f a">..</span>@ with the @class@ attribute
 -- given by the annotation function.
-displayHTML :: Monoid o => (String -> o) -> (a -> String) -> SimpleDoc a -> o
+displayHTML :: Monoid o => (Text -> o) -> (a -> Text) -> SimpleDoc a -> o
 displayHTML f g = displayDecorated push pop str
   where push t = f "<span class=\"" `mappend` f (g t) `mappend` f "\">"
         pop    = const $ f "</span>"
@@ -105,52 +110,52 @@ displayHTML f g = displayDecorated push pop str
 --
 -- The annotated region is wrapped by @<span class="f a">..</span>@ with the @class@ attribute
 -- given by the annotation function.
-displayHTMLT :: (a -> String) -> SimpleDoc a -> TL.Text
-displayHTMLT f = TL.toLazyText . displayHTML TL.fromString f
+displayHTMLT :: (a -> Text) -> SimpleDoc a -> TL.Text
+displayHTMLT f = TL.toLazyText . displayHTML TL.fromText f
 
 -- | Display a rendered document as HTML and output 'ByteString'.
 --
 -- The annotated region is wrapped by @<span class="f a">..</span>@ with the @class@ attribute
 -- given by the annotation function.
-displayHTMLB :: (a -> String) -> SimpleDoc a -> BL.ByteString
-displayHTMLB f = BL.toLazyByteString . displayHTML BL.stringUtf8 f
+displayHTMLB :: (a -> Text) -> SimpleDoc a -> BL.ByteString
+displayHTMLB f = BL.toLazyByteString . displayHTML T.encodeUtf8Builder f
 
 -- | Display a rendered document as HTML and output a 'ShowS' function.
 --
 -- The annotated region is wrapped by @<span class="f a">..</span>@ with the @class@ attribute
 -- given by the annotation function.
-displayHTMLS :: (a -> String) -> SimpleDoc a -> ShowS
-displayHTMLS f = (++) . displayHTML id f
+displayHTMLS :: (a -> Text) -> SimpleDoc a -> ShowS
+displayHTMLS f = (++) . displayHTML T.unpack f
 
 -- | Display a rendered document with ANSI escape sequences and output a 'Monoid'.
 --
 -- The annotations are mapped to a @[SetStyle]@ array.
-displayStyleCode :: Monoid o => (String -> o) -> (a -> [SetStyle]) -> Term -> SimpleDoc a -> o
+displayStyleCode :: Monoid o => (Text -> o) -> (a -> [SetStyle]) -> Term -> SimpleDoc a -> o
 displayStyleCode f g t d = runStyle t $
   mappend <$>
   displayDecoratedA push pop str d <*>
-  (f <$> applyStyleCode)
+  (f . T.pack <$> applyStyleCode)
   where push  x = changeStyle (Save:g x) >> pure mempty
         pop   _ = changeStyle [Restore]  >> pure mempty
-        str   s = f . (`mappend` s) <$> applyStyleCode
+        str   s = f . (`mappend` s) . T.pack <$> applyStyleCode
 
 -- | Display a rendered document with ANSI escape sequences and output a 'ShowS' function.
 --
 -- The annotations are mapped to a '[SetStyle]' array.
 displayStyleCodeS :: (a -> [SetStyle]) -> Term -> SimpleDoc a -> ShowS
-displayStyleCodeS f term = (++) . displayStyleCode id f term
+displayStyleCodeS f term = (++) . displayStyleCode T.unpack f term
 
 -- | Display a rendered document with ANSI escape sequences and output 'Text'.
 --
 -- The annotations are mapped to a '[SetStyle]' array.
 displayStyleCodeT :: (a -> [SetStyle]) -> Term -> SimpleDoc a -> TL.Text
-displayStyleCodeT f term = TL.toLazyText . displayStyleCode TL.fromString f term
+displayStyleCodeT f term = TL.toLazyText . displayStyleCode TL.fromText f term
 
 -- | Display a rendered document with ANSI escape sequences and output 'ByteString'.
 --
 -- The annotations are mapped to a '[SetStyle]' array.
 displayStyleCodeB :: (a -> [SetStyle]) -> Term -> SimpleDoc a -> BL.ByteString
-displayStyleCodeB f term = BL.toLazyByteString . displayStyleCode BL.stringUtf8 f term
+displayStyleCodeB f term = BL.toLazyByteString . displayStyleCode T.encodeUtf8Builder f term
 
 -- | Display a rendered document with ANSI escape sequences to a given 'Handle'.
 --
@@ -160,7 +165,7 @@ hDisplayStyle h f d = hRunWithStyle h [] $
   displayDecoratedA push pop str d >> applyStyle
   where push  x = changeStyle (Save:f x)
         pop   _ = changeStyle [Restore]
-        str   s = applyStyle >> liftIO (hPutStr h s)
+        str   s = applyStyle >> liftIO (T.hPutStr h s)
 
 -- | Display a rendered document with ANSI escape sequences to 'stdout'.
 --
